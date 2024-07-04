786774167b52e0d58a9fdf37ab0d6b0e299c955f
chrisbrunet <132419309+chrisbrunet@users.noreply.github.com>
2024-07-04 13:24:56 -0600

    added basic login query to db

--- streamlitapp/pages/account/login.py
+++ streamlitapp/pages/account/login.py
@@ -1,7 +1,23 @@
 import streamlit as st
+import pandas as pd
+import utils.streamlit_utils as stutil
 
-st.write("Please Click the Button to Log In")
+st.set_page_config(layout="centered")
+
+st.write("Enter Login Details Below:")
+
+email = st.text_input("Email")
+password = st.text_input("Password", type='password')
 
 if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
+        if email != '' and password != '':
+                users = stutil.get_collection('users')
+                query = users.find({"email": email, "password": password})
+                query_result = pd.DataFrame(query)
+                if query_result.empty:
+                        st.warning("Invalid Email/Password")
+                else:
+                        st.session_state.logged_in = True
+                        st.rerun()
+        else:
+                st.warning("Please Enter an Email and Password")
